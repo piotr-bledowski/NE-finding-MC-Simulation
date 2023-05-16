377dcac04ed895684d47eeda343cb7e8ea1c5bff
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-05-16 11:34:44 +0200

    Finished implementing runSimulation in Simulated Annealing

--- helpers.py
+++ helpers.py
@@ -1,5 +1,13 @@
 import numpy as np
+from game import TwoPlayerGame
 
 # Ensuring that an array sums to 1, i.e. is a probability distribution
 def normalize(arr: np.array) -> np.array:
     return arr / np.sum(arr)
+
+# This can either be standard deviation or variance of opponent's expected payoffs
+# as Nash Equilibrium occurs when said SD or variance is equal to 0
+def cost(game: TwoPlayerGame) -> float:
+    expected_payoffs1 = game.expectedPayoffs(1)
+    expected_payoffs2 = game.expectedPayoffs(2)
+    return np.var(expected_payoffs1) + np.var(expected_payoffs2)
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from game import TwoPlayerGame
-from helpers import normalize
+from helpers import normalize, cost
+from copy import deepcopy
 
 class SimulatedAnnealing:
     def __init__(self, game: TwoPlayerGame, n_epochs: int = 100, initial_temp: float = 100, final_temp: float = 0.01, cooling_rate: float = 0.01, step: str = 'fixed', step_size: float = 0.01):
@@ -18,23 +19,27 @@ class SimulatedAnnealing:
 
         while temp >= self.final_temp:
             for _ in range(self.n_epochs):
-                current_cost = self.cost()
-
-    # This can either be standard deviation or variance of opponent's expected payoffs
-    # as Nash Equilibrium occurs when said SD or variance is equal to 0
-    def cost(self) -> float:
-        expected_payoffs1 = self.game.expectedPayoffs(1)
-        expected_payoffs2 = self.game.expectedPayoffs(2)
-        return np.var(expected_payoffs1) + np.var(expected_payoffs2)
+                current_cost = cost(self.game)
+                # take a step, i.e. change one of the probabilities in one of the players' strategy
+                next_state = self.nextStep()
+                next_cost = cost(next_state)
+                delta = next_cost - current_cost
+                # always accept better states
+                if delta > 0:
+                    self.game = next_state
+                # if the new state is worse, accept it with probability derived from current temperature
+                elif np.exp(delta/temp) > np.random.rand(0, 1):
+                    self.game = next_state
 
     # Three possible approaches here
     # 1. taking a step of a fixed size
     # 2. taking a step from a uniform distribution (regular random walk)
     # 3. taking a step from a normal distribution (Gaussian random walk)
-    def nextStep(self):
+    def nextStep(self) -> TwoPlayerGame:
+        new_game = deepcopy(self.game)
         # randomly choosing a player
         player = np.random.randint(2) + 1
-        strategy = self.game.getStrategy(player)
+        strategy = new_game.getStrategy(player)
         # randomly choose an action
         action = np.random.randint(strategy.length)
         if self.step == 'fixed':
@@ -45,4 +50,5 @@ class SimulatedAnnealing:
         elif self.step == 'normal':
             strategy[action] = np.random.normal(loc=strategy[action], scale=self.step_size)
         strategy = normalize(strategy)
-        self.game.updateStrategy(player, strategy)
+        new_game.updateStrategy(player, strategy)
+        return new_game
