c5b54252b7025fd3fb33b8ef8e105434fa178516
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-05-16 13:24:51 +0200

    Simulated Annealing finds Nash Equilibrium for battle of the sexes game

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+from simulated_annealing import SimulatedAnnealing
+from game import TwoPlayerGame
+import numpy as np
+from helpers import normalize
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+as1 = np.array([-1, 1])
+as2 = np.array([-1, 1])
+po1 = np.vstack((np.array([2, 0]), np.array([0, 1])))
+po2 = np.vstack((np.array([1, 0]), np.array([0, 2])))
+s1 = normalize(np.random.rand(2))
+s2 = normalize(np.random.rand(2))
 
+print(np.vstack((s1, s2)))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+game = TwoPlayerGame(as1, as2, po1, po2, s1, s2)
 
+simulation = SimulatedAnnealing(game, n_epochs=100, initial_temp=0.01, final_temp=0.000001, cooling_rate=0.000001)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+simulation.runSimulation()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(simulation.getResult())
+
+plt.plot(np.arange(len(simulation.error)), np.array(simulation.error))
+plt.show()
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -3,6 +3,7 @@ import numpy as np
 from game import TwoPlayerGame
 from helpers import normalize, cost
 from copy import deepcopy
+from numba import jit
 
 class SimulatedAnnealing:
     def __init__(self, game: TwoPlayerGame, n_epochs: int = 100, initial_temp: float = 100, final_temp: float = 0.01, cooling_rate: float = 0.01, step: str = 'fixed', step_size: float = 0.01):
@@ -13,23 +14,27 @@ class SimulatedAnnealing:
         self.cooling_rate = cooling_rate
         self.step = step
         self.step_size = step_size
+        self.error = []
 
+    @jit(cache=True)
     def runSimulation(self):
         temp = self.initial_temp
 
         while temp >= self.final_temp:
             for _ in range(self.n_epochs):
                 current_cost = cost(self.game)
+                self.error.append(current_cost)
                 # take a step, i.e. change one of the probabilities in one of the players' strategy
                 next_state = self.nextStep()
                 next_cost = cost(next_state)
-                delta = next_cost - current_cost
+                delta = current_cost - next_cost
                 # always accept better states
                 if delta > 0:
                     self.game = next_state
                 # if the new state is worse, accept it with probability derived from current temperature
-                elif np.exp(delta/temp) > np.random.rand(0, 1):
+                elif np.exp(delta/temp) > np.random.rand():
                     self.game = next_state
+            temp -= self.cooling_rate
 
     # Three possible approaches here
     # 1. taking a step of a fixed size
@@ -41,14 +46,21 @@ class SimulatedAnnealing:
         player = np.random.randint(2) + 1
         strategy = new_game.getStrategy(player)
         # randomly choose an action
-        action = np.random.randint(strategy.length)
+        action = np.random.randint(strategy.size)
         if self.step == 'fixed':
             strategy[action] += np.random.choice([-self.step_size, self.step_size])
         elif self.step == 'uniform':
             # here self.step_size serves as a max possible step size
-            strategy[action] += np.random.rand(-self.step_size, self.step_size)
+            strategy[action] += np.random.uniform(-self.step_size, self.step_size)
         elif self.step == 'normal':
             strategy[action] = np.random.normal(loc=strategy[action], scale=self.step_size)
         strategy = normalize(strategy)
         new_game.updateStrategy(player, strategy)
+        # in case a probability goes below 0
+        if strategy[action] < 0:
+            return self.game
         return new_game
+
+    # return the Nash Equilibrium approximation found
+    def getResult(self) -> np.ndarray:
+        return np.vstack((self.game.getStrategy(1), self.game.getStrategy(2)))
